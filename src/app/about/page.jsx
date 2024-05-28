4536ff9db7c0cbd99036d93f4a350b08c1d261dd
Trongg2203 <138137607+Trongg2203@users.noreply.github.com>
2024-05-28 11:49:04 +0700

    create about page

---
+++ src/app/about/page.jsx
@@ -0,0 +1,26 @@
+export default function About() {
+  return (
+    <div className=" max-w-6xl mx-auto p-3 space-y-4">
+      <h1 className=" text-2xl font-medium text-amber-600">About</h1>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Qui, minus
+        atque iure sequi quis ipsum! Doloremque, ipsa ipsum sunt recusandae
+        asperiores molestiae labore nisi explicabo.
+      </p>
+
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Accusantium
+        culpa perspiciatis placeat accusamus, autem hic illo impedit, aliquam
+        iusto delectus dicta blanditiis debitis, eligendi corrupti beatae quia
+        odit? Consectetur doloribus consequuntur placeat animi vel velit sit
+        eaque, debitis dolores, cumque et blanditiis? Illo, nulla! Est inventore
+        mollitia repellendus cumque incidunt pariatur officia atque nesciunt
+        praesentium repellat. Neque, ipsa ut. Dolores neque minus, vitae cumque
+        fuga tempora alias similique consectetur maxime debitis! Fugit at ipsa
+        error ratione nemo blanditiis corrupti magnam obcaecati rerum.
+        Consequuntur, ab. Nam officiis dignissimos nisi unde totam molestiae,
+        odit harum sed. Laudantium laboriosam sint voluptatum. Libero, eos.
+      </p>
+    </div>
+  );
+}
